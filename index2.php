c84bd2ad57349437280220564820ad657c135ed3
Emersson Mendoza <emersson126@gmail.com>
2019-08-02 17:54:17 -0500

    Rename index.html to index2.php

---
+++ index2.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="EmerssonMendoza">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Fully functional and complete ICO Token sale Dashboard">
+	<title>EmerssonMendoza / es</title>
+    <link rel="shortcut icon" href="https://emerssonmendoza.com/assets/images/favicon.html">
+    <link rel="stylesheet" href="https://emerssonmendoza.com/assets/css/vendor.bundlead8f.css?ver=20190716111">
+    <link rel="stylesheet" href="https://emerssonmendoza.com/assets/css/stylead8f.css?ver=20190716111">
+<!-- EmerssonMendoza v11120190716. Application Developed by EmerssonMendoza -->
+</head>
+
+<body class="page-intro page-intro-bg">
+   
+    <div class="page-intro-split">
+        <div class="page-intro-nav">
+            
+            <a class="page-intro-logo" href="">
+                <img src="https://emerssonmendoza.com/assets/img/logo-light.png" alt="EmerssonMendoza">
+            </a>
+            
+            <a target="_blank" href="https://emprenedu.com" class="btn btn-auto btn-danger">CAMPUS VIRTUAL</a>
+        </div>
+        <div class="page-intro-side page-intro-side-a bg-secondary text-white">
+            <div class="text-block text-center wide-max-xs wide-auto">
+                <h2 class="page-intro-title"><span class="text-white">Emersson Mendoza</span> <small class="text-white fs-14 ucap">Experto en Plataformas Digitales</small></h2>
+                <p class="lead">Ingeniero de Sistemas <strong>UNIVERSIDAD NACIONAL DE INGENIERIA</strong><BR> CEO DE <strong>EmprenEDU.com</strong>.</p>
+                <div class="gaps-4x"></div>
+                <a target="_blank" href="https://wa.me/51958425493" class="btn btn-lg btn-success">WHATSAPP</a>
+                <div class="gaps-2x"></div>
+                <div class="hint hint-primary">* <strong>Version Demo</strong> Algunas funciones estan restringidas.</div>
+            </div>
+            <div class="page-intro-notice text-warning text-center">
+                <p>If you would like to <strong>try private/personal demo</strong>, Please send an email at <a href="mailto:emersson.mendoza.g@uni.pe">emersson.mendoza.g@uni.pe</a></p>
+            </div>
+        </div>
+        <div class="page-intro-side page-intro-side-b">
+            <div class="text-block text-center wide-max-xs wide-auto">
+                <h2 class="page-intro-title"><span>PORTAFOLIO</span> <small class="text-dark fs-14 ucap">VER PERFIL</small></h2>
+                <p class="lead">+89 Proyectos con <strong>Experiencia</strong> y <strong>Confianza</strong> para ti.</p>
+                <div class="gaps-4x"></div>
+                <a target="_blank" href="https://EmerssonMendoza.com/CV" class="btn btn-lg btn-primary">PORTAFOLIO</a>
+                <div class="gaps-2x"></div>
+                <div class="hint hint-secondary">* Gestion de Proyectos Informaticos.</div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
